1d0fcf31feca3116757ae39d857b32e5ca26ff5b
chotixity <melkychoti@gmail.com>
2024-03-25 11:21:24 +0300

    Empty project

---
+++ lib/home.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+  const MyHomePage({required this.title, super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
